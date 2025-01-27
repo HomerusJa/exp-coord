e5b162e2fcd6c209a4113099bb18f00c1f9e1708
HomerusJa <jakob@schluse.com>
2025-01-27 18:35:39 +0100

    :sparkles: Kept on working

--- src/exp_coord/services/s3i/broker.py
+++ src/exp_coord/services/s3i/broker.py
@@ -47,32 +47,18 @@ class S3IBrokerClient(AsyncClient):
         model_class: PydanticModel,
         endpoint: str,
         entity_type: str,
-    ) -> Optional[T]:
+    ) -> Optional[PydanticModel]:
         """Process a single entity response."""
         text = await response.text()
         if not text:
             logger.info(f"No {entity_type}s available for endpoint {endpoint}.")
             return None
         if isinstance(model_class, BaseModel):
-            return model_class.validate_json(text)
-        elif isinstance(model_class, TypeAdapter):
             return model_class.model_validate_json(text)
+        elif isinstance(model_class, TypeAdapter):
+            return model_class.validate_json(text)
         else:
             raise ValueError("Model class must be a Pydantic model or a Type Adapter.")
-
-    async def _process_multiple_response(
-        self,
-        response: AsyncResponse,
-        model_class: type[T],
-        endpoint: str,
-        entity_type: str,
-    ) -> list[T]:
-        """Process a multiple entities response."""
-        text = await response.text()
-        if text == "[]":
-            logger.info(f"No {entity_type}s available for endpoint {endpoint}.")
-            return []
-        return Multiple[model_class].model_validate_json(text).items
 
     async def get_message(self, endpoint: str) -> Optional[S3IMessage]:
         """Get a message from the broker."""
@@ -84,15 +70,8 @@ class S3IBrokerClient(AsyncClient):
     @endpoint("/{endpoint}", private, body(error=get_error))
     async def _get_message(self, endpoint: str) -> AsyncResponse: ...
 
-    async def get_all_messages(self, endpoint: str) -> list[S3IMessage]:
-        """Get all messages from the broker."""
-        response = await self._get_all_messages(endpoint=endpoint)
-        return await self._process_multiple_response(
-            response, S3IMessage, endpoint, "message"
-        )
-
     @endpoint("/{endpoint}/all", private, body(error=get_error))
-    async def _get_all_messages(self, endpoint: str) -> AsyncResponse: ...
+    async def get_all_messages(self, endpoint: str) -> list[S3IMessage]: ...
 
     async def get_event(self, endpoint: str) -> Optional[S3IEvent]:
         """Get an event from the broker."""
@@ -104,15 +83,8 @@ class S3IBrokerClient(AsyncClient):
     @endpoint("/{endpoint}/event", private, body(error=get_error))
     async def _get_event(self, endpoint: str) -> AsyncResponse: ...
 
-    async def get_all_events(self, endpoint: str) -> list[S3IEvent]:
-        """Get all events from the broker."""
-        response = await self._get_all_events(endpoint=endpoint)
-        return await self._process_multiple_response(
-            response, S3IEvent, endpoint, "event"
-        )
-
     @endpoint("/{endpoint}/event/all", private, body(error=get_error))
-    async def _get_all_events(self, endpoint: str) -> AsyncResponse: ...
+    async def get_all_events(self, endpoint: str) -> list[S3IEvent]: ...
 
     # TODO: Implement response handling.
     # The response is pretty detailed and should be handled properly.
