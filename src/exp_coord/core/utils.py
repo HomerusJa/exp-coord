fd37c1ad11d4641d0e6aa4887480e20f1b09eac2
HomerusJa <jakob@schluse.com>
2025-04-11 19:10:15 +0200

    :memo: Add note regarding a bug with unhashable types in _get_type_adapter

--- src/exp_coord/core/utils.py
+++ src/exp_coord/core/utils.py
@@ -10,5 +10,6 @@ T = TypeVar("T")
 @cache
 def _get_type_adapter(type_: T) -> TypeAdapter[T]:
     """Create a cached TypeAdapter instance for a type. The caching is the only reason for this function to exist."""
+    # FIXME: If type_ is not hashable, the cache will raise a TypeError
     logger.debug(f"Creating cached TypeAdapter for {type_}")
     return TypeAdapter(type_)
